b1b00b6750ed213cf6817baf3ca5e58325084c69
Shih Hao Lin <only520you2002@gmail.com>
2016-09-01 21:34:26 +0800

    Guset Query layout

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,37 @@
-require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
+import {Tabs, Tab, Checkbox} from 'react-bootstrap';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 
-let yeomanImage = require('../images/yeoman.png');
+const options = [
+    { value: 'one', label: 'One' },
+    { value: 'two', label: 'Two' }
+];
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+      <div className="container">
+        <Tabs defaultActiveKey={1} id="uncontrolled-tab-example">
+		    <Tab eventKey={1} title="人員登錄">
+		    	<div className="container indent">
+		    		<div className="align-middle">
+		    			<Select
+			    			clearable
+			    			options={options}
+			    		/>
+			    		<Checkbox validationState="success" className="margin-top">
+					    	<p className="font-setting">王大明2(同事)<span className="number-font">123</span>桌</p>
+					    </Checkbox>
+		    		</div>
+		    	</div>
+		    </Tab>
+		    <Tab eventKey={2} title="桌次一覽">
+		    	
+		    </Tab>
+		</Tabs>
       </div>
     );
   }
